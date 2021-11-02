125fb0bbebacda7e1dfddfe8b9b0058c7da8cf7e
Josh MillerÂ <josh@truemiller.com>
2021-11-02 22:53:03 +0000

    public logos

--- src/logos.js
+++ src/logos.js
@@ -1,69 +1,36 @@
-import aave from "./logos/aave.png"
-import avax from "./logos/avax.png"
-import ave from "./logos/ave.png"
-import avme from "./logos/avme.png"
-import bag from "./logos/bag.png"
-import bch from "./logos/bch.png"
-import btc from "./logos/btc.png"
-import cnr from "./logos/cnr.png"
-import cycle from "./logos/cycle.png"
-import dregg from "./logos/dregg.png"
-import ela from "./logos/ela.png"
-import elk from "./logos/elk.png"
-import eth from "./logos/eth.png"
-import gaj from "./logos/gaj.png"
-import gdl from "./logos/gdl.png"
-import joe from "./logos/joe.png"
-import link from "./logos/link.png"
-import lyd from "./logos/lyd.png"
-import mfi from "./logos/mfi.png"
-import mim from "./logos/mim.png"
-import pefi from "./logos/pefi.png"
-import png from "./logos/png.png"
-import qi from "./logos/qi.png"
-import snob from "./logos/snob.png"
-import set from "./logos/set.png"
-import susd from "./logos/susd.png"
-import sushi from "./logos/sushi.png"
-import teddy from "./logos/teddy.png"
-import wbtc from "./logos/wbtc.png"
-import xrp from "./logos/xrp.png"
-import yts from "./logos/yts.png"
-import usdt from "./logos/usdt.png"
-
 const Logos = {
-    aave:aave,
-    avax:avax,
-    ave:ave,
-    avme: avme,
-    bag:bag,
-    bch:bch,
-    btc:btc,
-    cnr:cnr,
-    cycle: cycle,
-    dregg: dregg,
-    ela: ela,
-    elk: elk,
-    eth: eth,
-    gaj: gaj,
-    gdl: gdl,
-    joe: joe,
-    link: link,
-    lyd: lyd,
-    mfi: mfi,
-    mim: mim,
-    pefi: pefi,
-    png: png,
-    qi: qi,
-    snob: snob,
-    sushi:sushi,
-    teddy:teddy,
-    wbtc: wbtc,
-    xrp:xrp,
-    yts:yts,
-    set:set,
-    susd:susd,
-    usdt:usdt
+    aave: "/logos/aave.png",
+    avax: "/logos/avax.png",
+    ave: "/logos/ave.png",
+    avme: "/logos/avme.png",
+    bag: "/logos/bag.png",
+    bch: "/logos/bch.png",
+    btc: "/logos/btc.png",
+    cnr: "/logos/cnr.png",
+    cycle: "/logos/cycle.png",
+    dregg: "/logos/dregg.png",
+    ela: "/logos/ela.png",
+    elk: "/logos/elk.png",
+    eth: "/logos/eth.png",
+    gaj: "/logos/gaj.png",
+    gdl: "/logos/gdl.png",
+    joe: "/logos/joe.png",
+    link: "/logos/link.png",
+    lyd: "/logos/lyd.png",
+    mfi: "/logos/mfi.png",
+    mim: "/logos/mim.png",
+    pefi: "/logos/pefi.png",
+    png: "/logos/png.png",
+    qi: "/logos/qi.png",
+    snob: "/logos/snob.png",
+    set: "/logos/set.png",
+    susd: "/logos/susd.png",
+    sushi: "/logos/sushi.png",
+    teddy: "/logos/teddy.png",
+    wbtc: "/logos/wbtc.png",
+    xrp: "/logos/xrp.png",
+    yts: "/logos/yts.png",
+    usdt: "/logos/usdt.png",
 }
 
 export default Logos
